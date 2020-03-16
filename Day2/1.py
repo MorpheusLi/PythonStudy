a9239d85e424b13ec47f8be45c716248ae1f3058
Morpheus <lmf870110@163.com>
2020-03-16 22:34:17 +0800

    Day2 study

---
+++ Day2/1.py
@@ -0,0 +1,15 @@
+"""
+使用变量保存并进行算数运算
+version:0.1
+Author:Morpheus
+"""
+
+a = 321
+b = 123
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a ** b)
---
+++ Day2/2.py
@@ -0,0 +1,61 @@
+"""
+使用input()函数获取键盘输入(字符串)
+使用int()函数将输入的字符串转换成整数
+使用print()函数输出带占位符的字符串
+
+Version: 0.1
+Author: Morpheus
+"""
+
+"""
+a = int(input('a = '))
+b = int(input('b = '))
+print('%d + %d = %d' % (a, b, a + b))
+print('%d - %d = %d' % (a, b, a - b))
+print('%d * %d = %d' % (a, b, a * b))
+print('%d / %d = %f' % (a, b, a / b))
+print('%d // %d = %d' % (a, b, a // b))
+print('%d %% %d = %d' % (a, b, a % b))
+print("%d ** %d = %d" % (a, b, a ** b))
+"""
+"""
+将华氏温度转换为摄氏温度
+
+Version: 0.1
+Author: Morpheus
+"""
+"""
+f = float(input('请输入华氏温度: '))
+c = (f - 32) / 1.8
+print('%.1f华氏度 = %.1f摄氏度' % (f, c))
+"""
+"""
+输入半径计算圆的周长和面积
+
+Version: 0.1
+Author: Morpheus
+"""
+
+"""
+import math
+
+radius = float(input('请输入圆的半径: '))
+perimeter = 2 * math.pi * radius
+area = math.pi * radius * radius
+print('周长: %.2f' % perimeter)
+print('面积: %.2f' % area)
+
+"""
+
+"""
+输入年份 如果是闰年输出True 否则输出False
+
+Version: 0.1
+Author: Morpheus
+"""
+
+year = int(input('请输入年份: '))
+# 如果代码太长写成一行不便于阅读 可以使用\对代码进行折行
+is_leap = (year % 4 == 0 and year % 100 != 0) or \
+           year % 400 == 0
+print(is_leap)
